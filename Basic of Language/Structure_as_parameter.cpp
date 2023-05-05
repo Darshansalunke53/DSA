0c755f69ea858bc4c4e376beef80677b76bd93bf
Darshansalunke53 <darshansalunke53@gmail.com>
2023-05-06 01:20:57 +0530

    Structure as Parameter

--- Basic of Language/Structure_as_parameter.cpp
+++ Basic of Language/Structure_as_parameter.cpp
@@ -18,12 +18,35 @@ int add_value(Rectangle s)
     
 }
 
+void change_value(Rectangle *s)
+{
+    s->a=100;
+    s->b=200;
+}
+
+void swap_value(Rectangle &s)
+{
+    int temp{};
+
+    temp=s.a;
+    s.a=s.b;
+    s.b=temp;
+}
+
 int main()
 
 {
     Rectangle r;
 
-    cout<<add_value(r);
+    cout<<add_value(r)<<endl;
+
+    change_value(&r);
+
+    cout<<r.a<<" "<<r.b<<endl;
+
+    swap_value(r);
+
+    cout<<r.a<<" "<<r.b<<endl;
 
 
     return 0;
